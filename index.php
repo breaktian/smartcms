6f568a25b8a28db0f4e2a5aae1fa4511cd3574c2
breaktian <breaktian@163.com>
2015-10-12 17:59:25 +0800

    采用ThinkPHP 模块化架 构思想

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 // 定义应用目录
-define('APP_PATH','./Home/');
+define('APP_PATH','./Cms/');
 
 //设定目录生成的文件
 define('DIR_SECURE_FILENAME', 'index.html');
