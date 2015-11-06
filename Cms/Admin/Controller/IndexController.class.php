ee394fd85417390ccc7cc1d05f6737f715084715
breaktian <breaktian@163.com>
2015-11-06 20:22:03 +0800

    20151106

--- Cms/Admin/Controller/IndexController.class.php
+++ Cms/Admin/Controller/IndexController.class.php
@@ -1,9 +1,84 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
 namespace Admin\Controller;
 use Think\Controller;
+
+/**
+ * 后台首页
+ * */
 class IndexController extends Controller {
+    
+    /**
+     * 后台首页
+     * */
     public function index(){
-	$this->show('Admin','utf-8');
+	   $this->display();
     }
+    
+    /**
+     * 后台首页top
+     * */
+    public function top(){
+        
+        $value = '蜡笔小新';
+        $this->assign('name',$value);
+        $this->display();
+    }
+    
+    /**
+     * 后台首页sidebar
+     * */
+    public function sidebar(){
+        $this->display();
+    }
+    
+    /**
+     * 后台首页main
+     * */
+    public function main(){
+        $data['php_os'] = PHP_OS;
+        $data['remote_addr'] = $_SERVER['REMOTE_ADDR'];
+        $data['server_software'] = $_SERVER['SERVER_SOFTWARE'];
+        $data['sql_version'] = mysql_get_server_info();
+        $this->assign('data', $data);
+        $this->display();
+    }
+    
+    /**
+     * 管理员管理
+     * */
+    public function admin(){
+        $this->display();
+    }
+    
+    /**
+     * 新增管理员
+     * */
+    public function addAdmin(){
+        $this->display();
+    }
+    
+    /**
+     * 角色管理
+     * */
+    public function role(){
+        echo "角色管理";
+    }
+    
+    /**
+     * 分类管理
+     * */
+    public function category(){
+        echo "分类管理";
+    }
+    
+    
+    /**
+     * 注销登录
+     * */
+    public function logout(){
+        $this->show('注销登录，跳转到登陆页','utf-8');
+        
+    }
+    
+    
 }
--- Cms/Common/Conf/config.php
+++ Cms/Common/Conf/config.php
@@ -1,4 +1,42 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	
+    
+    //设置默认主题目录
+    'DEFAULT_THEME'=>'default',
+    
+    // 站点公共目录
+    '__PUBLIC__'    =>  __ROOT__.'/Public',
+    
+    //页面Trace
+    'SHOW_PAGE_TRACE' =>false,
+    
+    
+    //修改定界符
+    'TMPL_L_DELIM'=>'<{',
+    'TMPL_R_DELIM'=>'}>',
+    
+    
+    //mysql全局定义
+    /*
+     'DB_TYPE'=>'mysql',
+     'DB_HOST'=>'localhost',
+     'DB_USER'=>'root',
+     'DB_PWD'=>'123456',
+     'DB_NAME'=>'thinkphp',
+     'DB_PORT'=>3306,
+     'DB_PREFIX'=>'think_',
+     */
+    //PDO专用定义
+    'DB_TYPE'=>'pdo',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'',
+    'DB_PREFIX'=>'smart_',
+    'DB_DSN'=>'mysql:host=localhost;dbname=smartcms;charset=UTF8',
+    
+    //字段缓存
+    'DB_FIELDS_CACHE'=>false
+    
+    
 );
